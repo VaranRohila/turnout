759c426b2fba3d2226d07a9156964846617c3e44
VaranRohila <varansinghrohila@gmail.com>
2018-07-07 02:25:43 +0530

    added sign_out button

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,6 +1,19 @@
 <template >
   <div class="">
-    Dashboard Component
-    {{$store.state.user}}
+    <h3>Events</h3>
+    <button type="button" @click='signOut' class="btn btn-danger btn-sm signout-btn ">Sign Out</button>
   </div>
 </template>
+
+<script>
+import { firebaseApp} from '../firebaseApp'
+
+export default {
+  methods: {
+    signOut() {
+      this.$store.dispatch('signOut')
+      firebaseApp.auth().signOut()
+    }
+  }
+}
+</script>
--- src/store/mutations.js
+++ src/store/mutations.js
@@ -4,5 +4,8 @@ import * as types from './mutation-types'
 export const mutations = {
   [types.SIGN_IN] (state, user_payload) {
     state.user = user_payload
+  },
+  [types.SIGN_OUT] (state) {
+    state.user = {}
   }
 }
